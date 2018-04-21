b387bac0c835eb5b60d67dc3f29a362505ce16c0
killghost15 <killghost15@manager>
2018-04-21 16:25:24 +0100

    ignore index.js it's the right thought wrong implementation

---
+++ dummy.js
@@ -0,0 +1,11 @@
+
+
+var io=require('socket.io');
+var socket = io.connect('http://localhost:8080');
+
+/*var Cylon = require("cylon");
+
+
+var device;
+ device = io('http://127.0.0.1:3000/api/robots/rosie/devices/led');
+ device.emit(''); */
